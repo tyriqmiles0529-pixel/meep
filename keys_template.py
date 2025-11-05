09ee2a5f8e1a163db74cbeb0ba5bec8cdffa2141
Test <test@example.com>
2025-11-05 16:11:07 -0600

    Add keys template and import in riq_analyzer

---
+++ keys_template.py
@@ -0,0 +1,20 @@
+"""
+API Keys - DO NOT COMMIT THIS FILE
+Add to your environment or import in scripts
+"""
+
+import os
+
+# API-Sports
+os.environ["API_SPORTS_KEY"] = "4979ac5e1f7ae10b1d6b58f1bba01140"
+os.environ["APISPORTS_KEY"] = "4979ac5e1f7ae10b1d6b58f1bba01140"
+
+# StatmilkGO/SportMonks
+os.environ["SGO_API_KEY"] = "3ee00eb314b80853c6c77920c5bf74f7"
+
+# The Odds API
+os.environ["THEODDS_API_KEY"] = "c98703301e8f89ef2c3648a4373939fd"
+
+# Kaggle (if needed locally - Colab users upload kaggle.json instead)
+# os.environ["KAGGLE_USERNAME"] = "your_username"
+# os.environ["KAGGLE_KEY"] = "your_key"
